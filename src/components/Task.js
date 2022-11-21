931c7370652e12c70d3a7d2ca99512e453e3d222
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2022-11-21 16:43:41 -0300

    mapeamento das tarefas

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,34 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View, ScrollView } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
 export default function Task() {
+  const [isTask, setIsTask] = useState([
+    {
+      id: 1,
+      todo: 'tesk 1',
+    },
+    {
+      id: 2,
+      todo: 'tesk 2',
+    },
+  ]);
+
   return (
     <ScrollView>
-      {/* tasks */}
-      <View style={styles.tasks}>
-        <Text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla convallis, erat sit amet
-          pellentesque ornare, velit ligula eleifend nunc, eget interdum dolor nibh viverra nulla.
-          Fusce nec felis at nulla finibus egestas vitae id orci. Suspendisse molestie lectus vel
-          nisi luctus, in malesuada justo auctor. Vivamus posuere condimentum varius.
-        </Text>
-        <View>
-          <TouchableOpacity>
-            <Feather name="trash" size={24} color="red" />
-          </TouchableOpacity>
-        </View>
-      </View>
-
-      {/* tasks */}
-      <View style={styles.tasks}>
-        <Text>Uma tarefa de exemplo</Text>
-        <View>
-          <TouchableOpacity>
-            <Feather name="trash" size={24} color="red" />
-          </TouchableOpacity>
-        </View>
-      </View>
+      {isTask.map((val) => {
+        return (
+          <View style={styles.tasks}>
+            <Text>{val.todo}</Text>
+            <View>
+              <TouchableOpacity>
+                <Feather name="trash" size={24} color="red" />
+              </TouchableOpacity>
+            </View>
+          </View>
+        );
+      })}
     </ScrollView>
   );
 }
